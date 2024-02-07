0a49abfe8092a16226c031cbe510879ccdaa5b0e
Ignacio Villafane <villafaneigna@gmail.com>
2024-02-07 17:11:38 -0300

    Mejorando navbar desktop y navbar mobile

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -7,37 +7,59 @@ const Navbar = () => {
         <img src={import.meta.env.VITE_IMAGE_1} alt="" className="logo-img" />
       </a>
       <div className="navbar-container-items">
-        <ul>
-          <li>
+        <ul className="navbar-container-items_menu">
+          <li className="items_menu">
             <a href="">
               Soy <span>Laura</span>
             </a>
           </li>
-          <li>
+
+          <li className="items_menu">
             <a href="">
               Mis <span>productos</span>
             </a>
+            <ul className="navbar-container-items_submenu">
+              <li className="items_submenu">
+                <a href="">La huerta</a>
+              </li>
+              <li className="items_submenu">
+                <a href="">Vivero circular</a>
+              </li>
+              <li className="items_submenu">
+                <a href="">Diseño y paisajismo</a>
+              </li>
+            </ul>
           </li>
-          <li>
+          <li className="items_menu">
             <a href="">
               Mis <span>servicios</span>
             </a>
+            <ul className="navbar-container-items_submenu">
+              <li className="items_submenu">
+                <a href="">La huerta</a>
+              </li>
+              <li className="items_submenu">
+                <a href="">Vivero circular</a>
+              </li>
+              <li className="items_submenu">
+                <a href="">Diseño y paisajismo</a>
+              </li>
+            </ul>
           </li>
 
-          <li>
+          <li className="items_menu">
             <a href="">
               <span>Proyectos</span> realizados
             </a>
           </li>
-          <li>
+          <li className="items_menu">
             <a href="">
               <span>Alianzas</span> activas
             </a>
           </li>
 
-          <li>
+          <li className="items_menu">
             <a href="">
-              {" "}
               Experiencias y <span>talleres</span>
             </a>
           </li>
--- src/components/layout/NavbarResponsive/NavbarMobile.jsx
+++ src/components/layout/NavbarResponsive/NavbarMobile.jsx
@@ -1,22 +1,77 @@
+import React, { useState } from "react";
 import "./NavbarMobile.css";
 import MenuIcon from "@mui/icons-material/Menu";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import CancelIcon from "@mui/icons-material/Cancel";
 
 const NavbarMobile = () => {
-  const Funciona = () => {
-    console.log("FUNCIONA!❤");
+  const [showSubmenu, setShowSubmenu] = useState(false);
+
+  const toggleSubmenu = () => {
+    setShowSubmenu(!showSubmenu);
+  };
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
   };
   return (
-    <div className="navbarMobile-container">
-      <img
-        src={import.meta.env.VITE_IMAGE_1}
-        alt="LogoMobile"
-        className="LogoMobile"
-      />
-      <div className="navbar-container_separador"></div>
-      <button className="btn-menu" onClick={Funciona}>
-        <MenuIcon fontSize="string" />
-      </button>
-    </div>
+    <>
+      <div className="navbarMobile-container">
+        <img
+          src={import.meta.env.VITE_IMAGE_1}
+          alt="LogoMobile"
+          className="LogoMobile"
+        />
+        <div className="navbar-container_separador"></div>
+        <button className="btn-menu" onClick={toggleMenu}>
+          <MenuIcon fontSize="string" />
+        </button>
+      </div>
+      <div className={`menu-items ${showMenu ? "show" : ""}`}>
+        <button onClick={toggleMenu}>
+          <CancelIcon />
+        </button>
+        <ul className="container-items">
+          <li className="menu-li">
+            <a href="">Soy Laura</a>
+          </li>
+          <li className="menu-li">
+            <a href="">Mis productos</a>
+          </li>
+          <li className="menu-li" onClick={toggleSubmenu}>
+            <a href="#">
+              Mis servicios{" "}
+              <ArrowDropDownIcon
+                fontSize="string"
+                sx={{ padding: "5px 0 0 0", fontSize: "25px" }}
+              />
+            </a>
+            <ul className="container-items_submenu">
+              <li className={`submenu-li ${showSubmenu ? "show" : ""}`}>
+                <a href="#">La huerta</a>
+              </li>
+              <li className={`submenu-li ${showSubmenu ? "show" : ""}`}>
+                <a href="#">Vivero circular</a>
+              </li>
+              <li className={`submenu-li ${showSubmenu ? "show" : ""}`}>
+                <a href="#">Diseño y paisajismo</a>
+              </li>
+            </ul>
+          </li>
+          <li className="menu-li">
+            <a href="">Proyectos realizados</a>
+          </li>
+          <li className="menu-li">
+            <a href="">Alianzas Activas</a>
+          </li>
+          <li className="menu-li">
+            <a href="">Experiencias y talleres</a>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
