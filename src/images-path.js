87a6112dc4fb76d4fb9720cbe8f92a1220084530
Ignacio Villafane <villafaneigna@gmail.com>
2024-02-17 21:01:53 -0300

    Ruta completa TEST

--- src/images-path.js
+++ src/images-path.js
@@ -1,7 +1,7 @@
 const imagePaths = {
-  logo: "./assets/img/tomillologo.png",
-  laura: "./assets/img/lauratomillo.jpg",
-  hola: "./assets/img/hola.png",
+  logo: "src/assets/img/tomillologo.png",
+  laura: "src/assets/img/lauratomillo.jpg",
+  hola: "src/assets/img/hola.png",
 };
 
 export default imagePaths;
